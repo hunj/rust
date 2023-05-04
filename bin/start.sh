be08169701f5a90c09459131a8f38f7db8896ffb
hunj <hunjaelee@protonmail.com>
2023-05-04 17:36:13 -0400

    remove quotes

--- bin/start.sh
+++ bin/start.sh
@@ -12,13 +12,13 @@ echo "Starting Rust.."
   +rcon.port $RUST_RCON_PORT \
   +rcon.password $RUST_RCON_PASSWORD \
   +server.maxplayers $RUST_SERVER_MAXPLAYERS \
-  +server.hostname $RUST_SERVER_HOSTNAME \
+  +server.hostname "$RUST_SERVER_HOSTNAME" \
   +server.identity $(whoami) \
-  +server.level $RUST_SERVER_LEVEL \
+  +server.level "$RUST_SERVER_LEVEL" \
   +server.seed $RUST_SERVER_SEED \
   +server.worldsize $RUST_SERVER_WORLDSIZE \
   +server.saveinterval $RUST_SERVER_SAVEINTERVAL \
-  +server.description $RUST_SERVER_DESCRIPTION \
-  +server.headerimage $RUST_SERVER_HEADERIMAGE \
-  +server.url $RUST_SERVER_URL
+  +server.description "$RUST_SERVER_DESCRIPTION" \
+  +server.headerimage "$RUST_SERVER_HEADERIMAGE" \
+  +server.url "$RUST_SERVER_URL"
   -logfile "log-$(date -I).log"
